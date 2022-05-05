125aa3221e5217862dbddbb489839157653cd6f3
fabricio <fabricio.alborta.22@gmail.com>
2022-05-05 18:36:06 -0400

    Mejora DB Firebase + Módulos

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@
 import React, {useEffect} from 'react';
 import { collection, getDocs } from "firebase/firestore";
 import firestore from './confs/firebaseConf';
-//import { async } from '@firebase/util';
+import { async } from '@firebase/util';
 
 const App= () =>{
   useEffect(() => {
 
-    const obtenerDatos = async () => {
-      const datos = await getDocs(collection(firestore,'UrnaVoto'))
-      console.log(datos);
-    } 
+    const obtenerDatos = async() =>{
+      const datos = await getDocs(collection(firestore, 'urnaElectoral'));
+      console.log(datos.docs);
+    }   
+
     obtenerDatos();
-  }, []);
+  },[]);
 
   return(
     <h1>Urna de Votos</h1>
--- src/confs/firebaseConf.js
+++ src/confs/firebaseConf.js
@@ -1,15 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCEnAfoWOUOWb3AcsCvME-NcaKl6Z9x6hQ",
-  authDomain: "elecciones-electorales-tss.firebaseapp.com",
-  projectId: "elecciones-electorales-tss",
-  storageBucket: "elecciones-electorales-tss.appspot.com",
-  messagingSenderId: "1066944497467",
-  appId: "1:1066944497467:web:f5b7d116ace8d46bf2991e",
-  measurementId: "G-8EJ3JYQ5HD"
+  apiKey: "AIzaSyAAeXLJOj4oTSFOsYTs123DVvDmz_MVSc4",
+  authDomain: "elecciones-tss.firebaseapp.com",
+  projectId: "elecciones-tss",
+  storageBucket: "elecciones-tss.appspot.com",
+  messagingSenderId: "344787203266",
+  appId: "1:344787203266:web:c98972e3b915807714775b",
+  measurementId: "G-8T2VLWTKNJ"
 };
 
 // Initialize Firebase
