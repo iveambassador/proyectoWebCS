7607415cbe2b3972c8f17588bf486709cf0c482c
Ivan <monzonyujraivanfranklin@gmail.com>
2022-05-05 18:07:45 -0400

    Problema Responsve Solucionado

--- src/Componets/Candidato.js
+++ src/Componets/Candidato.js
@@ -4,17 +4,17 @@ import logo192 from '../Images/logo512.png'
 import logo from '../logo.svg'
 import '../Styles/Candidato.css'
 // import logo from '../../public/logo192.png'
-export default function Candidato() {
+export default function Candidato({Partido, NombreAp, Cargo}) {
   return (
     <Card className='Cont-Card'>
-        <Card.Title>Nombre Del Partido Politico</Card.Title>
+        <Card.Title>{Partido}</Card.Title>
         <Card.Img variant="top" src={logo} alt='nada por aqui!'/>
         <Card.Body>
-          <Card.Title>Nombre: Juanito Perez</Card.Title>
+          <Card.Title>Nombre: {NombreAp}</Card.Title>
           <Card.Text>
-            Cargo: Secretario de Deportes
+            Cargo: {Cargo}
           </Card.Text>
-          <Form.Group >
+          <Form.Group className='test'>
             <Form.Check className='Checkbox'/>
           </Form.Group>
           {/* <Button variant="primary">Go somewhere</Button> */}
--- src/Pages/EmitirVoto.js
+++ src/Pages/EmitirVoto.js
@@ -11,14 +11,34 @@ export default function EmitirVoto() {
         <p>Si marcas mas de una casilla se contara como voto nulo</p>
       </div>
       <div className='Cont-Candidatos'>
-        <Candidato />
-        <Candidato />
-        <Candidato />
-        <Candidato />
-        <Candidato />
-        <Candidato />
-        <Candidato />
-        <Candidato />
+        <Candidato 
+        Partido='Juntos por el 100'
+        NombreAp='Juan Perez Sanchez'
+        Cargo='secretario de Deportes'/>
+        <Candidato Partido='Run Sistemas'
+         NombreAp='Eduardo Vale martinez'
+         Cargo='Presidente '/>
+        <Candidato 
+        Partido='Code Sistemas'
+        NombreAp='David Oropeza Cordova'
+        Cargo='Secretario de Actas'/>
+        <Candidato 
+        Partido='Los Vengadores de Notas!'
+        NombreAp='Harol Zurita Simon'
+        Cargo='Documentador de Informes'/>
+        <Candidato 
+        Partido='No mas Salteñas'
+        NombreAp='Pepe Pancho Suarez'
+        Cargo='Presidente de Asociasion'/>
+        <Candidato 
+         Partido='El mas weno que puedas ver'
+         NombreAp='Jhonny Fulano Choque'
+         Cargo='Vicepresidente de Asociasion'/>
+        <Candidato 
+        Partido='Se me acabaron los names'
+        NombreAp='Erika Jhaelis Soto Diaz'
+        Cargo='Testing Qa de Notas'/>
+        
       </div>
     </div>
   )
