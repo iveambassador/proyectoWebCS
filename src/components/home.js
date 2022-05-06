f787a36619746d23aa6e0acf13b4476b9c4d22d5
Rodrigo Ledezma <rodri.ledezma9483@gmail.com>
2022-05-06 18:44:07 -0400

    crear pie y bar

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import Pie from './charts/pie'
+import Bar from './charts/bar'
 
 
 export default class home extends Component {
+
   render() {
     return (
-      <div>
-          <h1>Inicio</h1>
-      </div>
+        <div>
+            <Pie/>
+            <Bar/>
+        </div>
     )
   }
 }
