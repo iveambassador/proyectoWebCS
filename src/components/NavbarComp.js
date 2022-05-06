b810b95bb3b56d9c339b85ec050bbe6d1ff2ba83
Rodrigo Ledezma <rodri.ledezma9483@gmail.com>
2022-05-06 15:01:12 -0400

    Crear un menÃu bar

---
+++ src/components/NavbarComp.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './home'
+import Urna from './urna'
+import Register from './register'
+import Login from './login'
+
+export default class NavbarComp extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+            <Navbar bg="dark" variant={"dark"} expand="lg">
+              <Container fluid>
+                  <Navbar.Brand as={Link} to={"/home"}>Ivote</Navbar.Brand>
+                  <Navbar.Toggle aria-controls="navbarScroll" />
+                  <Navbar.Collapse id="navbarScroll">
+                  <Nav
+                      className="me-auto my-2 my-lg-0"
+                      style={{ maxHeight: '100px' }}
+                      navbarScroll
+                  >
+                      <Nav.Link as={Link} to={"/home"}>Inicio</Nav.Link>
+                      <Nav.Link as={Link} to={"/urna"}>Urna Electoral</Nav.Link>
+                  </Nav>
+                  <Nav>
+                    <Nav.Link as={Link} to={"/login"}>Iniciar Sesión</Nav.Link>
+                    <Nav.Link as={Link} to={"/register"}>Registrarse</Nav.Link>
+                  </Nav>
+                  </Navbar.Collapse>
+              </Container>
+              </Navbar>
+              <div>
+                <Routes>
+                  <Route path="/home" element={<Home/>}/>
+                  <Route path="/urna" element={<Urna/>}/>
+                  <Route path="/login" element={<Login/>}/>                    
+                  <Route path="/register" element={<Register/>}/>
+                </Routes>
+              </div>
+        </div>
+      </Router>
+    )
+  }
+}
---
+++ src/components/urna.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class urna extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Urna Electoral</h1>
+      </div>
+    )
+  }
+}
