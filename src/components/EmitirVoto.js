e4294a483a75c3b22168297ed6eee644434d196e
Hanyinson Alex <calex1731@gmail.com>
2022-05-10 17:12:58 -0400

    Generador de Hash en el boton Votar

--- src/components/EmitirVoto.js
+++ src/components/EmitirVoto.js
@@ -1,61 +1,77 @@
-import React from 'react'
-import Candidato from './Candidato'
-import '../Styles/EmitirVoto.css'
-import { Button } from 'react-bootstrap'
+import React from "react";
+import Candidato from "./Candidato";
+import "../Styles/EmitirVoto.css";
+import { Button } from "react-bootstrap";
+
+const SHA256 = require("crypto-js/sha256");
 
 export default function EmitirVoto() {
+  function hash() {
+    let hashGenerado = SHA256(JSON.stringify()).toString();
+    console.log(hashGenerado);
+  }
   return (
-    <div className='Container'>
-      
+    <div className="Container">
       <h1>Votar</h1>
-      <div className='Cont-Titulo'>
-        <div className='tester'>
-        <h5>Marca al candidato de tu preferencia.</h5>
-        <h5>Puedes votar en blaco si esa es tu elecion.</h5>
-        <h5>Si marcas mas de una casilla se contara como voto "nulo".</h5>
+      <div className="Cont-Titulo">
+        <div className="tester">
+          <h5>Marca al candidato de tu preferencia.</h5>
+          <h5>Puedes votar en blaco si esa es tu elecion.</h5>
+          <h5>Si marcas mas de una casilla se contara como voto "nulo".</h5>
         </div>
-        <div className='testers'></div>
-        <div className='testers'></div>
+        <div className="testers"></div>
+        <div className="testers"></div>
       </div>
-      <div className='Cont-Candidatos'>
-        <Candidato 
-        Partido='Juntos por el 100'
-        NombreAp='Juan Perez Sanchez'
-        Cargo='secretario de Deportes'/>
-        <Candidato Partido='Run Sistemas'
-         NombreAp='Eduardo Vale martinez'
-         Cargo='Presidente '/>
-        <Candidato 
-        Partido='Code Sistemas'
-        NombreAp='David Oropeza Cordova'
-        Cargo='Secretario de Actas'/>
-        <Candidato 
-        Partido='Los Vengadores de Notas!'
-        NombreAp='Harol Zurita Simon'
-        Cargo='Documentador de Informes'/>
-        <Candidato 
-        Partido='No mas Salteñas'
-        NombreAp='Pepe Pancho Suarez'
-        Cargo='Presidente de Asociasion'/>
-        <Candidato 
-         Partido='El mas weno que puedas ver'
-         NombreAp='Jhonny Fulano Choque'
-         Cargo='Vicepresidente de Asociasion'/>
-        <Candidato 
-        Partido='Se me acabaron los names'
-        NombreAp='Erika Jhaelis Soto Diaz'
-        Cargo='Testing Qa de Notas'/>
-        <Candidato 
-        Partido='Se me acabaron los names'
-        NombreAp='Erika Jhaelis Soto Diaz'
-        Cargo='Testing Qa de Notas'/>
-        <Candidato 
-        Partido='No mas Salteñas'
-        NombreAp='Pepe Pancho Suarez'
-        Cargo='Presidente de Asociasion'/>
-        
+      <div className="Cont-Candidatos">
+        <Candidato
+          Partido="Juntos por el 100"
+          NombreAp="Juan Perez Sanchez"
+          Cargo="secretario de Deportes"
+        />
+        <Candidato
+          Partido="Run Sistemas"
+          NombreAp="Eduardo Vale martinez"
+          Cargo="Presidente "
+        />
+        <Candidato
+          Partido="Code Sistemas"
+          NombreAp="David Oropeza Cordova"
+          Cargo="Secretario de Actas"
+        />
+        <Candidato
+          Partido="Los Vengadores de Notas!"
+          NombreAp="Harol Zurita Simon"
+          Cargo="Documentador de Informes"
+        />
+        <Candidato
+          Partido="No mas Salteñas"
+          NombreAp="Pepe Pancho Suarez"
+          Cargo="Presidente de Asociasion"
+        />
+        <Candidato
+          Partido="El mas weno que puedas ver"
+          NombreAp="Jhonny Fulano Choque"
+          Cargo="Vicepresidente de Asociasion"
+        />
+        <Candidato
+          Partido="Se me acabaron los names"
+          NombreAp="Erika Jhaelis Soto Diaz"
+          Cargo="Testing Qa de Notas"
+        />
+        <Candidato
+          Partido="Se me acabaron los names"
+          NombreAp="Erika Jhaelis Soto Diaz"
+          Cargo="Testing Qa de Notas"
+        />
+        <Candidato
+          Partido="No mas Salteñas"
+          NombreAp="Pepe Pancho Suarez"
+          Cargo="Presidente de Asociasion"
+        />
       </div>
-      <Button variant="primary" size='lg' className='mb-4'>Guardar Voto!</Button>
+      <Button onClick={hash} variant="primary" size="lg" className="mb-4">
+        Guardar Voto!
+      </Button>
     </div>
-  )
+  );
 }
