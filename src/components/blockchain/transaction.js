c7d49c6fcb2ff1f5bb30732b84a69298d5fde3a5
fabricio <fabricio.alborta.22@gmail.com>
2022-05-27 21:06:17 -0400

    mejoras en la transacción

--- src/components/blockchain/transaction.js
+++ src/components/blockchain/transaction.js
@@ -3,11 +3,11 @@ const SHA1 = require('crypto-js/sha1');
 const SHA256 = require('crypto-js/sha256');
 const HmacSHA1 = require('crypto-js/hmac-sha1');
 const HmacSHA256 = require('crypto-js/hmac-sha256');
-
 const CryptoJS = require('crypto-js');
+const { Transaction } = require('firebase/firestore');
 
 // sse introduce y utiliza un solo algoritmo
-let message = 'Heng y Mo H jaja 1234343434';
+let message = 'son parte del Hash';
 let key = 'hengyumo@1234';
 
 console.log('md5: ' + MD5(message));
@@ -15,7 +15,6 @@ console.log('sha1: ' + SHA1(message));
 console.log('sha256: ' + SHA256(message));
 console.log('hmac-sha1: ' + HmacSHA1(message, key));
 console.log('hmac-sha256: ' + HmacSHA256(message, key));
-
 
 // todo el uso, debido a que aes contiene cifrado y descifrado, hay dos métodos, no puede usar AES directamente para cifrar
 let result = CryptoJS.AES.encrypt(message, key);
@@ -30,12 +29,11 @@ console.log('des decrypt: ' + CryptoJS.DES.decrypt(result, key).toString(CryptoJ
 console.log('hmac-sha256: ' + CryptoJS.HmacSHA256(message, key));
 console.log('sha256: ' + CryptoJS.SHA256(message));
 
-
 // en base 32, codificación hexadecimal hexadecimal para convertir el resultado del cifrado
 let base64Result = CryptoJS.enc.Base32.stringify(CryptoJS.DES.decrypt(result, key));
 console.log(base64Result);
 let hexResult = CryptoJS.enc.Hex.stringify(CryptoJS.DES.decrypt(result, key));
 console.log(hexResult);
 
-
+module.exports = Transaction;
 
