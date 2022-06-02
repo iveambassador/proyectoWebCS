133a51b3bca20fc5db0ead87d9bd2aa04ede7dd9
Ivan <monzonyujraivanfranklin@gmail.com>
2022-06-02 09:05:40 -0400

    implementacion captura de datos firebase

--- src/components/PagePostulante.js
+++ src/components/PagePostulante.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import '../Styles/Postulante.css'
 import Postulante from './Postulante'
+import { firestore } from "../confs/firebaseConf";
+import { app } from "../confs/firebaseConf";
+import {collection,query, where, getDocs, getFirestore, updateDoc, doc} from "firebase/firestore";
+//import { async } from '@firebase/util';
 export default function PagePostulantes() {
+
+  //componentDidMount() { this.getBooks() }
+  async function test(){
+    const q = query(collection(firestore, "UsuarioComun"), where("PostularEstado", "==", true));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+  }
+  test();
+   console.log("Se imprime!!!")
   return (
-    
     <div className="contMain">
       <div className='contPostulante'>
         <h4 className='contenedor-testimonio w-100'>Postulantes: </h4>
