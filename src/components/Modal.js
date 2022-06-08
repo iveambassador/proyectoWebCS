eac5032d39c7c9b70fea92e00f227522f9f776cf
fabricio <fabricio.alborta.22@gmail.com>
2022-06-08 07:29:16 -0400

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,48 +4,23 @@ import { Modal, Button } from 'react-bootstrap'
 //import Blockchain from './blockchain/blockchain';
 //import block from './blockchain/block';
 
-//const SHA256 = require('crypto-js/sha256')
+const SHA256 = require('crypto-js/sha256')
 const Blockchain = require('./blockchain/blockchain');
 const Block = require('./blockchain/block');
 
 export default function Modales(props) {
   async function generar (){
-    //let hashGenerado = SHA256(JSON.stringify()).toString()
-    //console.log(hashGenerado)
-    // hashGenerado = Math.random()
-    //return hashGenerado
-    if (props.posi){
-      let blockchain = new Blockchain();
-      let block = new Block('hola soy un block', '0');
-      blockchain.addBlock(block);
-      console.log(blockchain);
-      if (blockchain.isChainValid()){
-        console.log('La cadena es valida');
-        while (blockchain.chain.length < 10){
-          let block = new Block('hola soy un block', blockchain.chain[blockchain.chain.length - 1].hash);
-          blockchain.addBlock(block);
-        }
-        console.log(blockchain);
-      }else{
-        console.log('La cadena no es valida');
-      }
-    }
-    
-    const blockchain = new Blockchain();
-    const blockInicio = new Block({data: "No lo sé Rodri, parece falso la SEMILLA"});
-    const blockFin = new Block({data: "GENESIS 1:26 o_O"});
-
-    await blockchain.addBlock(blockInicio);
-    await blockchain.addBlock(blockFin);
-
-    blockchain.print();
+    let hashGenerado = SHA256(JSON.stringify()).toString()
+    console.log(hashGenerado)
+    hashGenerado = Math.random()
+    return hashGenerado;
   }
 
-    function CambiarModal(){
-      props.onHide()
-      props.test()
-      props.setMensaje(`${generar()}`)
-    }
+  function CambiarModal(){
+    props.onHide()
+    props.test()
+    props.setMensaje(`${generar()}`)
+  }
 
     return (
       <Modal
