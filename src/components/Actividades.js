98b1bb1764fe63f291907b213e329ac71f76ffa5
The-Rigo <55820400+The-Rigo@users.noreply.github.com>
2022-06-15 15:21:33 -0400

    mejorar actividades

--- src/components/Actividades.js
+++ src/components/Actividades.js
@@ -7,7 +7,7 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import '../Styles/Login.css'
 import { useState, useEffect } from "react";
 import NoDisponible from './NoDisponible'
-import { Table } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
 
 
 export default function Actividades() {
@@ -31,7 +31,7 @@ useEffect(() => {
         let Hash = DatosSem.data().HashSemilla
         let HashPrevio = DatosSem.data().HashPrevio+'init'
         let Transaccion = DatosSem.data().Transaccion
-        let Fecha = (DatosSem.data().Fecha).toString()
+        let Fecha = (DatosSem.data().Fecha.toDate()).toString().substring(4,21)
         let dato = {Hash,HashPrevio,Transaccion,Fecha}
         listaTemp.push(dato);
         console.log(listaTemp)
@@ -71,26 +71,31 @@ useEffect(() => {
   
    
       return (
-          <Table striped bordered hover>
+        
+          <div>
+            <h3 className="text-center" style={{fontWeight:'bold'}}>Actividades: </h3>
+          <Table striped bordered hover responsive style={{ maxWidth:'1000px' ,marginLeft:'auto', marginRight:'auto', border:'2px solid #757a96'}} >
     <thead>
-      <tr>
-        <th>Hash</th>
-        <th>HashPrevio</th>
-        <th>Transaccion</th>
-        <th>Fecha</th>
+      <tr >
+        <th class="text-center mb-4 mt-2">Hash</th>
+        <th class="text-center mb-4 mt-2">HashPrevio</th>
+        <th class="text-center mb-4 mt-2">Transaccion</th>
+        <th class="text-center mb-4 mt-2">Fecha</th>
       </tr>
     </thead>
     <tbody>
     { lista.map(tupla => (
       <tr>
-        <td>{tupla.Hash}</td>
-        <td>{tupla.HashPrevio}</td>
-        <td>{tupla.Transaccion}</td>
-        <td>{tupla.Fecha}</td>
+        <td class="text-center mb-4 mt-2">{tupla.Hash}</td>
+        <td class="text-center mb-4 mt-2">{tupla.HashPrevio}</td>
+        <td class="text-center mb-4 mt-2">{tupla.Transaccion}</td>
+        <td class="text-center mb-4 mt-2">{tupla.Fecha}</td>
       </tr>
        )) }  
     </tbody>
 </Table>
+
+</div>
       );
   
       return (
